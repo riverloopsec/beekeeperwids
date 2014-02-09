2d09ef493d8624d7d6caec200de432191f42e243
Javier Vazquez <javier@riverloopsecurity.com>
2014-02-09 13:57:07 -0500

    Created /app/wids/monitor.py

---
+++ trunk/apps/wids/monitor.py
@@ -0,0 +1,90 @@
+'''
+This app is intended to monitor a network for specific packets. When a packet meeting a specific filter is detected,
+and event alert is generated
+'''
+
+class MonitorApp:
+
+	def __init__(self):
+		pass
+
+
+	def callback(self):
+		pass
+
+
+	def storeData(self):
+		pass
+
+
+	def getData(self):
+		pass
+
+
+	def loop(self):
+		pass
+
+
+
+class AnalyticModule:
+
+	def __init__(self):
+		pass
+
+
+	def start(self):
+		pass
+
+
+	def stop(self):
+		pass
+
+
+def ReportingModule:
+
+	def __init__(self):
+		pass
+
+
+	def sendAlert(self):
+		pass
+
+
+class DataListener:
+
+	def __init__(self):
+		pass
+
+
+	def startServer(self):
+		pass
+
+
+	def stopServer(self):
+		pass
+
+
+	def recvData(self):
+		pass
+
+	def saveData(self):
+		pass
+
+
+
+
+class DatabaseHandler:
+
+	def __init__(self):
+		pass
+
+	def dataPush(self, data):
+		pass
+
+	def dataPull(self):
+		pass
+
+	def ifNewData(self):
+		pass
+
+
