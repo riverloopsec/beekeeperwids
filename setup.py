1654ad289c002762f3c711291797edc95d30a4eb
Ryan Speers <ryan@riverloopsecurity.com>
2014-03-02 12:41:31 -0500

    added killerbee 2.5+ to required packages

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python
+
+try:
+    from setuptools import setup
+except ImportError:
+    print("No setuptools found, attempting to use distutils instead.")
+    from distutils.core import setup
+
+setup(name='killerbeewids',
+      version='0.1',
+      description='Wireless Intrusion Detection System for 802.15.4/ZigBee',
+      author='TBD',
+      author_email='javier@riverloopsecurity.com',
+      url='http://www.riverloopsecurity.com',
+      packages=['killerbeewids'],
+      install_requires=['flask', 'killerbee >= 2.5'],
+      # Consider using Markdown such as in https://coderwall.com/p/qawuyq
+      long_description = open('README.txt').read(),
+     )
+
