5dda809f286c8b36a2c7392882ae178bc42eebbd
Ryan Speers <ryan@riverloopsecurity.com>
2014-03-02 21:25:47 -0500

    add some logging

--- killerbeewids/wids/modules/beaconreqscan.py
+++ killerbeewids/wids/modules/beaconreqscan.py
@@ -42,12 +42,14 @@ class BeaconRequestMonitor(AnalyticModule):
             datetime_now  = datetime.utcnow()
             datetime_t30  = datetime_now - timedelta(seconds=30)
             datetime_t120 = datetime_now - timedelta(seconds=120)
-            p30  = self.getPackets(queryFilter=[('datetime','>',dateToMicro(datetime_t30))])
+            p30  = self.getPackets(queryFilter=[('datetime','>',dateToMicro(datetime_t30))],
+                                   uuid=[uuid_task1])
             p120 = self.getPackets(queryFilter=[('datetime','<',dateToMicro(datetime_t30 )),
-                                                ('datetime','>',dateToMicro(datetime_t120))])
+                                                ('datetime','>',dateToMicro(datetime_t120))],
+                                   uuid=[uuid_task1])
             n30  = len(p30)
-            an90 = len(p120)/3.0 #30-120 seconds is a 90 second range so 3 x 30sec intervals
-            self.logutil.log("Found {0} beacon requests in last 30 seconds, and {1} per 30 secs average over the prior 90 seconds.".format(n30, an90))
+            an90 = len(p120)/3.0 #30-120 seconds is a 90 second range so 3 * 30sec intervals
+            self.logutil.log("Found {0} beacon requests in last 30 seconds, and {1} per 30 secs average over the prior 90 seconds (absolute {2}).".format(n30, an90, len(p120)))
             # Every N scans, or every time a new "block" of scans occurs,
             #   rasise an "informational level" event.
             #TODO
@@ -55,11 +57,6 @@ class BeaconRequestMonitor(AnalyticModule):
             # Calculate a moving average of how many of these we typically
             #     see in a given time, and if we're significantly higher
             #     than that all of a sudden, we're concerned.
-            #TODO query the DB for the number of packets matching our UUID
-            #   in the past 30 seconds, and then query the DB for the # of
-            #   packets which were seen between t-30 seconds ago and t-150
-            #   seconds ago / 5. Seeing the last 30 seconds higher than the
-            #   average of the previous time detects a spike.
             try:
                 ratio = float(n30)/an90
                 if ratio > 1.5:
